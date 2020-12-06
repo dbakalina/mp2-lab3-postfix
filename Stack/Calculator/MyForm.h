df750d6117a3699d6fdeb8e613cc9afe29a665bd
dbakalina <darya.bakalina@gmail.com>
2020-12-06 22:41:17 +0300

    Add Graphic Calculator

--- Stack/Calculator/MyForm.h
+++ Stack/Calculator/MyForm.h
@@ -41,6 +41,9 @@ namespace Calculator {
 	protected:
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
 
 
 
@@ -61,6 +64,9 @@ namespace Calculator {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// textBox1
@@ -89,11 +95,41 @@ namespace Calculator {
 			this->label1->TabIndex = 2;
 			this->label1->Text = L" ";
 			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(17, 23);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(113, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Введите выражение:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(17, 87);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(116, 13);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"Постфиксная форма:";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(17, 119);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(10, 13);
+			this->label4->TabIndex = 5;
+			this->label4->Text = L" ";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(496, 298);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
@@ -122,7 +158,7 @@ namespace Calculator {
 		calc.ToPostfix();
 		double res = calc.CalcPostfix();
 		label1->Text = Convert::ToString(res);
-		//label1->Text = msclr::interop::marshal_as<String^>(calc.GetPostfix());
+		label4->Text = msclr::interop::marshal_as<String^>(calc.GetPostfix());
 
 	}
 	private: System::Void textBox1_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
